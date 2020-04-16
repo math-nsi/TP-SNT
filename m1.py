29a52fcab8ac4940c968d12916831d7a9eeb28f6
math-nsi <63494300+math-nsi@users.noreply.github.com>
2020-04-16 11:46:45 +0200

    Add files via upload

--- m1.py
+++ m1.py
@@ -35,7 +35,7 @@ def c5(f):
 # Fonction qui valide le TP6
 def c6(f):
     test=True
-    for i in range(1,300):
+    for i in range(50,110):
         if i<80:
             m=i
         else:
@@ -43,31 +43,16 @@ def c6(f):
         if m!=f(i):
             test=False
     affrep(test)
-
-
-# Fonction qui valide le TP3
-def ctp11(f):
+    
+# Fonction qui valide le TP7
+def c7(f):
     test=True
-    for i in range(1,10):
-        if i<8:
-            m=11*i+10
+    for i in range(1,30):
+        if 11*i<60:
+            m=11*i+8
         else:
             m=11*i
         if m!=f(i):
             test=False
     affrep(test)
-        
-# Fonction qui valide le TP4
-def ctp12(f):
-    test=True
-    for i in range(1,300):
-        if i<=80:
-            m=i
-        else: 
-            if i<150 :
-                m=0.94*i
-            else:
-                m=0.85*i
-        if m!=f(i):
-            test=False
-    affrep(test)
+
